efb3c49609b073415de0ae6f2a781ea73d306694
Richard McCormack <brick@brick.codes>
2016-11-02 11:39:41 -0400

    Basic client capability - we can connect now! Lots of refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -2,60 +2,11 @@
 
 #![plugin(clippy)]
 
+extern crate byteorder;
 extern crate openssl;
 extern crate protobuf; // depend on rust-protobuf runtime
 
-use std::net::{IpAddr, TcpStream};
-
-use openssl::ssl::{SslContext, SslMethod, SslStream};
-use openssl::ssl::HandshakeError;
+mod proto;
+pub mod client;
 
 pub const DEFAULT_PORT: u16 = 64738;
-const SSL_HANDSHAKE_RETRIES: u8 = 3;
-
-#[derive(Debug)]
-pub enum ConnectionError {
-    ExceededHandshakeRetries(&'static str),
-    Ssl(openssl::ssl::Error),
-    TcpStream(std::io::Error)
-} // TODO: this should impl error, display
-
-pub fn connect(host: IpAddr, port: u16) -> Result<SslStream<TcpStream>, ConnectionError> {
-    let mut context: SslContext;
-    match SslContext::new(SslMethod::Tlsv1) {
-        Ok(val) => context = val,
-        Err(err) => return Err(ConnectionError::Ssl(openssl::ssl::Error::from(err)))
-    }
-    // TODO: This will do no cert verification. We should have an option for this.
-    context.set_verify(openssl::ssl::SSL_VERIFY_NONE);
-    //context.set_verify(openssl::ssl::SSL_VERIFY_PEER);
-    let stream: TcpStream;
-    match TcpStream::connect((host, port)) {
-        Ok(val) => stream = val,
-        Err(err) => return Err(ConnectionError::TcpStream(err))
-    }
-    match SslStream::connect(&context, stream) {
-        Ok(val) => Ok(val),
-        Err(err) => match err {
-            HandshakeError::Failure(handshake_err) => Err(ConnectionError::Ssl(handshake_err)),
-            HandshakeError::Interrupted(interrupted_stream) => {
-                let mut x = interrupted_stream;
-                let mut tries: u8 = 1;
-                while tries < SSL_HANDSHAKE_RETRIES {
-                    match x.handshake() {
-                        Ok(val) => return Ok(val),
-                        Err(err) => match err {
-                            HandshakeError::Failure(handshake_err) => return Err(ConnectionError::Ssl(handshake_err)),
-                            HandshakeError::Interrupted(new_interrupted_stream) => {
-                                x = new_interrupted_stream;
-                                tries += 1;
-                                continue
-                            }
-                        }
-                    }
-                }
-                Err(ConnectionError::ExceededHandshakeRetries("Exceeded number of handshake retries"))
-            }
-        }
-    }
-}
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,11 +1,15 @@
 extern crate mumble;
 
+use mumble::client::MumbleClient;
+
+use std::{thread, time};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 #[test]
-fn test_connection() {
+fn test_all() {
     // Connect to a local murmur server
-    mumble::connect(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), mumble::DEFAULT_PORT).unwrap();
-    // Connect to the same server ipv6
-    mumble::connect(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), mumble::DEFAULT_PORT).unwrap();
+    let mut connection = MumbleClient::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), mumble::DEFAULT_PORT, "mumble-rs", "nil").unwrap();
+    loop {
+        thread::sleep(time::Duration::from_secs(1));
+    }
 }
