44ade5ffe07afe027574554686bd2ddee6f4cab7
Richard McCormack <brick@brick.codes>
2016-11-03 17:10:34 -0400

    Update test

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,14 +1,13 @@
 extern crate mumble;
 
-use mumble::Connection;
+use mumble::{Client, ClientOptions};
 
 use std::{thread, time};
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 #[test]
 fn test_connection() {
     // Connect to a local murmur server
-    let client = Connection::new(IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1)), mumble::DEFAULT_PORT, "mumble-rs", "nil").unwrap();
+    let client = Client::new(ClientOptions::default()).unwrap();
     loop {
         thread::sleep(time::Duration::from_secs(1));
     }
